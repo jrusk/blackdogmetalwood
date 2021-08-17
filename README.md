b5d9c1f9b4bed24aa5a56e0ad3c48d6c0b58fcab
Jonathan Rusk <jrusk@chromis.com>
2021-08-17 12:45:30 -0700

    Upgrade to Next 11, remove dep on Bulma etc (#12)

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 # Black Dog Metal and Wood
 
-Black Dog Metal and Wood public website built with [Next.js](https://nextjs.org/), [Bulma](https://bulma.io/), [`@zeit/next-sass`](https://github.com/zeit/next-plugins/tree/master/packages/next-sass).
+Black Dog Metal and Wood public website built with [Next.js](https://nextjs.org/).
 
 ## Running
 
@@ -18,5 +18,3 @@ To build and start a production version of the code, run:
 npm run build
 npm run start
 ```
-
-**Credit:** based on [jvarness/next-with-bulma](https://github.com/jvarness/next-with-bulma).
--- pages/index.js
+++ pages/index.js
@@ -1,63 +1,70 @@
 import Head from "next/head";
-import "../styles/styles.sass";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
 
-const Index = () => (
-  <div>
-    <Head>
-      <title>Black Dog Metal and Wood</title>
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      <link rel="icon" type="image/x-icon" href="/static/favicon.ico" />
-      <link
-        rel="stylesheet"
-        href="https://use.fontawesome.com/releases/v5.7.0/css/all.css"
-        integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ"
-        crossOrigin="anonymous"
-      />
-    </Head>
-    <section className="hero has-background-black is-fullheight">
-      <div className="hero-head">
-        <figure className="image is-3by1">
-          <img src="static/blackdog_logo.jpg" />
-        </figure>
-      </div>
-      <div className="hero-body">
-        <div className="has-text-left has-text-grey-lighter">
-          <h2 className="subtitle has-text-grey-lighter">
-            Custom Metal Creations
-          </h2>
-          <figure className="image">
-            <img src="static/table.jpg" />
-          </figure>
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Black Dog Metal and Wood</title>
+        <meta name="description" content="Black Dog Metal and Wood" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <div className={styles.hero}>
+          <Image
+            src="/blackdog_logo.jpg"
+            alt="Black Dog Logo"
+            layout="fill"
+            objectFit="contain"
+            objectPosition="center top"
+          />
         </div>
-      </div>
-      <div className="hero-foot has-background-black">
-        <p className="has-text-grey-lighter">
-          <strong className="has-text-grey">Location:</strong> Phoenix, AZ
+        <div>
+          <h1 className={styles.title}>Custom Metal Creations</h1>
+
+          <div className={styles.banner}>
+            <Image
+              src="/table.jpg"
+              alt="Black Dog Logo"
+              layout="fill"
+              objectFit="contain"
+              objectPosition="center top"
+            />
+          </div>
+        </div>
+      </main>
+
+      <footer className={styles.footer}>
+        <span>
+          <strong className={styles.textGray}>Location:</strong> Phoenix, AZ
           &nbsp;|&nbsp;
-          <strong className="has-text-grey">Tel:</strong> (623) 680-1376
+        </span>
+        <span>
+          <strong className={styles.textGray}>Tel:</strong> (623) 680-1376
           &nbsp;|&nbsp;
-          <strong className="has-text-grey">Email:</strong>{" "}
+        </span>
+        <span>
+          <strong className={styles.textGray}>Email: </strong>
           <a href="mailto:info@blackdogmetalwood.com" target="_top">
             info@blackdogmetalwood.com
           </a>
           &nbsp;|&nbsp;
-          <strong className="has-text-grey">Facebook:</strong>
+        </span>
+        <span>
+          <strong className={styles.textGray}>Facebook:</strong>
           <a href="https://www.facebook.com/blackdogmetalwood/">
-            <span className="icon">
-              <i className="fab fa-facebook-f" />
-            </span>
+            &nbsp;
+            <Image
+              src="/f_logo_RGB-Blue_58.png"
+              alt="Facebook Logo"
+              width={18}
+              height={18}
+            />
           </a>
-        </p>
-        <br />
-      </div>
-    </section>
-    <style jsx>{`
-      .has-bg-img {
-        background: url("static/blackdog_logo.jpg") center center;
-        background-size: cover;
-      }
-    `}</style>
-  </div>
-);
-
-export default Index;
+        </span>
+      </footer>
+    </div>
+  );
+}
